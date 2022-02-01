d3b40ba0fd66fac8b9511887eac73bcf7f8c87be
AmrEidAbdelrahman <amreid248@gmail.com>
2022-02-02 01:15:51 +0200

    start solve the problem from begining with reset to a savepoint

--- Eltahdy/settings.py
+++ Eltahdy/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-8(2c&wp@dp08ss^+3#=f0@^*6*@^k=r-c=fh=wx0xv(uftelvo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
